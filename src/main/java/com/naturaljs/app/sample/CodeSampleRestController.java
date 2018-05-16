8c5dd9c2d5b5490388c33e8b7301a2f26f814a38
JakarataKim <JakarataKim@DESKTOP-2HGPIG5>
2018-05-16 14:53:29 +0900

    .

--- src/main/java/com/naturaljs/app/sample/CodeSampleRestController.java
+++ src/main/java/com/naturaljs/app/sample/CodeSampleRestController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
